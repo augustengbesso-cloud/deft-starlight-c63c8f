237940357e58d98c43002c8b2afb614c23b0caae
augustengbesso-cloud <augustengbesso@gmail.com>
2026-08-15 16:29:40 +0000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "2na-erp-v2";
+const CACHE_NAME = "2na-erp-v3";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
